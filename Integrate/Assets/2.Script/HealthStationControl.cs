40d1cca2130289c010b343fc778c96f731ee1372
SidanFan <740213415@qq.com>
2019-01-24 19:28:51 -0500

    healingStation (unfinished)

---
+++ Integrate/Assets/2.Script/HealthStationControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthStationControl : MonoBehaviour {
+
+    [SerializeField] GameObject triggerText;
+    [SerializeField] GameObject VFX_healthStation;
+    [SerializeField] GameObject VFX_HealingPrefab;
+    [SerializeField] float healingValue = 100;
+    [SerializeField] bool isTriggered = false;
+
+    [SerializeField] PlayerControl playerControl;
+
+    void Start () {
+        playerControl = GameObject.Find("Player").GetComponent<PlayerControl>();
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player" && !isTriggered)
+        {
+            isTriggered = true;
+            playerControl.currentHP += healingValue;
+            if (playerControl.currentHP > playerControl.maxHP)
+            {
+                playerControl.currentHP = playerControl.maxHP;
+            }
+            GameObject temp_vfx = Instantiate(VFX_HealingPrefab, transform.position, Quaternion.identity);
+            Destroy(temp_vfx, 2.0f);
+            VFX_healthStation.SetActive(false);
+        }
+    }
+
+}
