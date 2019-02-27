e3370d136c0eebb4f5e862bce834d2624d850bf8
SidanFan <740213415@qq.com>
2019-02-27 14:09:19 -0500

    Add boundary prefab/death vfx

---
+++ Integrate/Assets/2.Script/LevelBoundary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBoundary : MonoBehaviour
+{
+    [SerializeField] PlayerControl playerControl;
+    [SerializeField] GameObject vfx_death; //drag the prefab!
+
+    private void Start()
+    {
+        playerControl = GameObject.Find("Player").GetComponent<PlayerControl>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            Instantiate(vfx_death, playerControl.gameObject.transform.position, Quaternion.identity);
+            playerControl.canControl = false;
+            playerControl.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            playerControl.gameObject.GetComponent<Collider>().enabled = false;
+            playerControl.ReloadFromDeath();
+            
+        } 
+    }
+}
