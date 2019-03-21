11486636e622967f09775dbd5f9d50caa430f2bd
SidanFan <740213415@qq.com>
2019-03-21 15:14:49 -0400

    Report data to Google Form

---
+++ Integrate/Assets/4.Prefab/ReportData/PreSurvey.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreSurvey : MonoBehaviour
+{
+    [SerializeField] string base_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeGbjyUWase4-kPxrQn-B5wvs1e5LOtTPnhKKjGt4L8wOXLhg/formResponse";
+    [SerializeField] string answer;
+
+    IEnumerator SendToForm(string answer)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("entry.1947089699", answer);
+        byte[] rawData = form.data;
+
+        WWW www = new WWW(base_URL, rawData);
+        yield return www;
+        //yield return new WaitForSecondsRealtime(1f);
+    }
+
+    public void Send()
+    {
+        StartCoroutine(SendToForm(answer));
+    }
+
+    public void SetAnswer(string selectedAnswerString)
+    {
+        answer = selectedAnswerString;
+        Send();
+    }
+}
