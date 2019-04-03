b791bbf4bafa9ddfeffcb2a325f8f01eb1db4d57
mghjin <kim.jinh@husky.neu.edu>
2019-04-03 13:40:00 -0500

    finish credit script in menu

---
+++ Integrate/Assets/2.Script/CreditsPanelScript.cs
@@ -0,0 +1,26 @@
+﻿/* 
+ * GSND 6320 PSYCHOLOGY OF PLAY
+ * PROJECT 1 DIGITAL PROTOTYPE
+ * CODERS:
+ * SIDAN FAN
+ * JIN H KIM 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsPanelScript : MonoBehaviour
+{
+    public GameObject CreditsPanel;
+    bool status = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            status = !status;
+            CreditsPanel.SetActive(status);
+        }
+    }
+}
